5dc05470b2794172db8f2e3a4ed0c7ec31e5ad73
Ali <ali.ozgur@example.com>
2023-04-13 12:21:35 +0300

    edit AddCourse component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+
 import './App.css'
+import AddCourse from './components/AddCourse';
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <div className="App bg-red-500">
+    <div className="App bg-slate-100 w-1/2 mx-auto mt-3">
       
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    
+       <AddCourse/>
      
     </div>
   );
--- src/components/AddCourse.jsx
+++ src/components/AddCourse.jsx
@@ -2,8 +2,33 @@ import React from 'react'
 
 const AddCourse = () => {
   return (
-    <div>AddCourse</div>
-  )
+    <div className="">
+      {" "}
+      <form className="flex flex-col gap-y-1 ">
+        <label htmlFor="kurs " className="font-small">
+          Kurs İsmi
+        </label>
+        <input
+          type="text"
+          className="max-w-[25rem] w-full mx-auto border p-1"
+          placeholder="Kurs ismini yazınız"
+          id="kurs"
+        />
+        <label htmlFor="ücret " className="sfont-small">
+          Kurs Ücreti
+        </label>
+        <input
+          type="text"
+          className="max-w-[25rem] w-full mx-auto border p-1"
+          placeholder="Kurs ücretinizi yazınız"
+          id="ücret"
+        />
+        <button className="p-1 text-sm bg-blue-500 text-yellow-300 mt-2 border-opacity-5">
+          Kaydet
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default AddCourse
